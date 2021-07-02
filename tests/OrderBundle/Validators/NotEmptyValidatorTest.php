5cd2e6db625cb4e0b2ddc7deed5b5e0a284de01a
Raphael Giron <maingiron@gmail.com>
2021-07-01 21:12:08 -0300

    add NotEmptyValidatorTest

---
+++ tests/OrderBundle/Validators/NotEmptyValidatorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\OrderBundle\Validators;
+
+use OrderBundle\Validators\NotEmptyValidator;
+use PHPUnit\Framework\TestCase;
+
+final class NotEmptyValidatorTest extends TestCase
+{
+    /** 
+     * @test 
+     * @dataProvider valueProvider
+     */
+    public function isValid($value, $expectedResult): void
+    {
+        $validator = new NotEmptyValidator($value);
+
+        $returned = $validator->isValid();
+
+        $this->assertEquals($expectedResult, $returned);
+    }
+
+    public function valueProvider()
+    {
+        return [
+            'shouldBeValidWhenValueIsNotEmpty' => ['value' => 'xpto', 'expectedResult' => true],
+            'shouldNotBeValidWhenValueIsEmpty' => ['value' => '', 'expectedResult' => false]
+        ];
+    }
+}
+
