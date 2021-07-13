d971a3ed74d6fed157b61111922ef69962d591e6
Raphael Giron <maingiron@gmail.com>
2021-07-12 23:14:49 -0300

    add PaymentServiceTest

---
+++ tests/PaymentBundle/Service/PaymentServiceTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\PaymentBundle\Service;
+
+use OrderBundle\Entity\CreditCard;
+use OrderBundle\Entity\Customer;
+use OrderBundle\Entity\Item;
+use PaymentBundle\Repository\PaymentTransactionRepository;
+use PaymentBundle\Service\Gateway;
+use PaymentBundle\Service\PaymentService;
+use PHPUnit\Framework\TestCase;
+
+final class PaymentServiceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldBeSaveWhenGatewayReturnedOkWithRetries()
+    {
+        $gatewayMock = $this->createMock(Gateway::class);
+        $gatewayMock->expects($this->atLeast(3))
+            ->method('pay')
+            ->will($this->onConsecutiveCalls(
+                false, false, true
+            ));
+
+        $repositoryMock = $this->createMock(PaymentTransactionRepository::class);
+        $repositoryMock->expects($this->once())
+            ->method('save');
+
+        $service = new PaymentService($gatewayMock, $repositoryMock);
+
+        $customerMock = $this->createMock(Customer::class);
+        $itemMock = $this->createMock(Item::class);
+        $creditCardMock = $this->createMock(CreditCard::class);
+
+        $service->pay($customerMock, $itemMock, $creditCardMock);
+    }
+}
