848b06a20c7cdbbc441a82781c49dcec761ba193
Raphael Giron <maingiron@gmail.com>
2021-07-14 21:20:35 -0300

    add shouldBeThrowExceptionWhenGatewayFailed

--- tests/PaymentBundle/Service/PaymentServiceTest.php
+++ tests/PaymentBundle/Service/PaymentServiceTest.php
@@ -7,6 +7,7 @@ namespace Tests\PaymentBundle\Service;
 use OrderBundle\Entity\CreditCard;
 use OrderBundle\Entity\Customer;
 use OrderBundle\Entity\Item;
+use PaymentBundle\Exception\PaymentErrorException;
 use PaymentBundle\Repository\PaymentTransactionRepository;
 use PaymentBundle\Service\Gateway;
 use PaymentBundle\Service\PaymentService;
@@ -29,6 +30,33 @@ final class PaymentServiceTest extends TestCase
         $repositoryMock = $this->createMock(PaymentTransactionRepository::class);
         $repositoryMock->expects($this->once())
             ->method('save');
+
+        $service = new PaymentService($gatewayMock, $repositoryMock);
+
+        $customerMock = $this->createMock(Customer::class);
+        $itemMock = $this->createMock(Item::class);
+        $creditCardMock = $this->createMock(CreditCard::class);
+
+        $service->pay($customerMock, $itemMock, $creditCardMock);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeThrowExceptionWhenGatewayFailed()
+    {
+        $gatewayMock = $this->createMock(Gateway::class);
+        $gatewayMock->expects($this->atLeast(3))
+            ->method('pay')
+            ->will($this->onConsecutiveCalls(
+                false, false, false
+            ));
+
+        $repositoryMock = $this->createMock(PaymentTransactionRepository::class);
+        $repositoryMock->expects($this->never())
+            ->method('save');
+
+        $this->expectException(PaymentErrorException::class);
 
         $service = new PaymentService($gatewayMock, $repositoryMock);
 
